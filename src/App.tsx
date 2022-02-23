2c33240ad59e2fcc92f3a11767bd4a5e37aa2cac
kalidan <mesfinkalkidan@gmail.com>
2022-02-23 22:55:38 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from 'react';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import axios from "axios"
 function App() {
+  var url = "https://www.admin.rechargeapps.com/oauth/token"
+  const data = {
+    code: "0xDEADBEEF",
+    grant_type: "authorization_code",
+    redirect_uri: "https://your_domain.com",
+    client_id: "32a0cd0eb1459e24bc104cb0a6d4b70421f21750f36d3ab6e7b550dbd10957e4"
+  }
+  const body = JSON.stringify(data)
+
+  const reachRecharge = async () => {
+    let res = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+    console.log("what will be logged", res)
+  }
+  useEffect(() => {
+    reachRecharge()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Welcome
     </div>
   );
 }
